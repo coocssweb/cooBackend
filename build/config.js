d6c6a4fe0ee7056b0f74e8fe7dcffb56d43fc4b5
coocssweb <1974740999@qq.com>
2020-11-11 01:29:23 +0800

    URL

--- build/config.js
+++ build/config.js
@@ -9,7 +9,7 @@ module.exports = {
   // 生产环境
   production: {
     env: "production", // 环境
-    api: "", // api 接口地址
+    api: "https://www.shuxia123.com/api/", // api 接口地址
     publicPath: "", // 静态资源地址
     imagePath: "", // 图片资源地址
     devtool: "false", // devtool
